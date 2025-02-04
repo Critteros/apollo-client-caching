f38905f75c6c2399af3a15c3acc0018ba700122f
Szymon Kozioł <szymon.koziol@deployed.pl>
2025-02-04 14:57:43 +0100

    Added shadcui components

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": resolve(fileURLToPath(new URL("./src", import.meta.url))),
+    },
+  },
+});
